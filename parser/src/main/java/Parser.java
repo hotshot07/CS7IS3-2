7780a1957560f8affa17dc394e212efef655530b
hotshot07 <aroram@tcd.ie>
2021-11-03 10:30:56 +0000

    structure created

---
+++ parser/src/main/java/Parser.java
@@ -0,0 +1,7 @@
+public class Parser {
+  // this class will be used to call all classes in the utils, like FbisDocumentParser where
+  // we'll pass the path to root content is
+  // data/data/fbis
+
+
+}
