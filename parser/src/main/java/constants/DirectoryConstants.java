50e7c447e39327b0e122dcc660346e45c360cf17
Chaitanya Joglekar <chaitanyajoglekar2@gmail.com>
2021-11-13 19:32:37 +0000

    Reverting change in data directory

--- parser/src/main/java/constants/DirectoryConstants.java
+++ parser/src/main/java/constants/DirectoryConstants.java
@@ -2,7 +2,7 @@ package constants;
 
 public class DirectoryConstants {
   public static final String INDEX_DIR = "data/index";
-  public static final String DATA_DIR = "data/";
+  public static final String DATA_DIR = "data/docs";
   public static final String RESULTS_DIR = "Results";
   public static final String TOPIC_PATH = "data/queries/topics";
 }
